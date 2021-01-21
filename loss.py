d00d00f1b0fd3c27df69d70eb3c0b7a2def30181
Raman Ghimire <rmnghmire07@gmail.com>
2021-01-21 13:52:23 +0900

    first commit

---
+++ loss.py
@@ -0,0 +1,24 @@
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+
+class DiceBCELoss(nn.Module):
+    def __init__(self, weight=None, size_average=True):
+        super(DiceBCELoss, self).__init__()
+
+    def forward(self, inputs, targets, smooth=1):
+        inputs = torch.sigmoid(inputs)
+
+        # Flatten label and prediction tensors
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+
+        intersection = (inputs * targets).sum()
+        dice_loss = 1 - (2. * intersection + smooth) / (inputs.sum() + targets.sum() + smooth)
+        BCE = F.binary_cross_entropy(inputs, targets, reduction='mean')
+        Dice_BCE = BCE + dice_loss
+
+        return Dice_BCE
---
+++ test.py
@@ -0,0 +1,26 @@
+import torch
+from torch.utils.data import DataLoader, Dataset
+from model import NestedUNet
+from torch.utils.data.sampler import RandomSampler
+from dataloader import random_seed, PolypDataset
+
+
+test_images_file = "data/test_images.txt"
+test_labels_file = "data/test_masks.txt"
+
+input_size = (128, 128)
+torch.manual_seed(7)
+test_set = PolypDataset(test_images_file, test_labels_file, input_size)
+test_loader = DataLoader(test_set, batch_size = 1, sampler = RandomSampler(test_set))
+
+# Inference device
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+# Load Model
+model_path = "experiment_test/polyp_unet1.pth"
+# model = NestedUNet(n_channels = 3, n_classes = 1, bilinear = False).to(device)
+model = NestedUNet((num_classes= 1, input_channels = 3, bilinear = False).to(device)
+
+model.load_state_dict(torch.load(model_path, map_location = device))
+model.eval()
+
